7389fdee1d8daa8cd3c47142363eead306d57218
anton <antonrumancev67@gmail.com>
2026-07-21 15:59:47 +0400

    feat: create home page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,27 +1,7 @@
-import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-950">
-      <div className="rounded-xl border border-slate-800 bg-slate-900 p-8 shadow-xl">
-        <h1 className="text-3xl font-bold text-white">Mini Discord</h1>
-
-        <p className="mt-2 text-slate-400">
-          React, Vite and Tailwind are working.
-        </p>
-
-        <button
-          className="
-            mt-6 rounded-lg bg-indigo-600 px-4 py-2
-            font-medium text-white transition-colors
-            hover:bg-indigo-500
-          "
-        >
-          Sign in
-        </button>
-      </div>
-    </main>
-  );
+  return <Home />;
 }
 
 export default App;
