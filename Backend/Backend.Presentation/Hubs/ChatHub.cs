bc381f0b57824b8c2af0547048f35381846f8e7b
anton <antonrumancev67@gmail.com>
2026-08-23 14:18:47 +0300

    feat: add subscribeToChat method to chat hub

--- Backend/Backend.Presentation/Hubs/ChatHub.cs
+++ Backend/Backend.Presentation/Hubs/ChatHub.cs
@@ -1,4 +1,6 @@
+using Backend.Application.Chats.RequestHandlers.Queries;
 using Backend.Application.SignalR.Interfaces;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
@@ -8,6 +10,30 @@ namespace Backend.Presentation.Hubs;
 /// SignalR hub for chat functionality.
 /// </summary>
 [Authorize]
-public class ChatHub : Hub<IChatClient>
+public class ChatHub(IMediator mediator) : Hub<IChatClient>
 {
+    public async Task SubscribeToChat(Guid chatId)
+    {
+        string? clerkId =
+            Context.User?.FindFirst("sub")?.Value;
+
+        if (string.IsNullOrEmpty(clerkId))
+        {
+            throw new HubException(
+                "User is not authenticated.");
+        }
+
+        await mediator.Send(
+            new GetChatInfoQuery.Query(
+                clerkId,
+                chatId),
+            Context.ConnectionAborted);
+
+        string groupName = $"chat:{chatId:N}";
+
+        await Groups.AddToGroupAsync(
+            Context.ConnectionId,
+            groupName,
+            Context.ConnectionAborted);
+    }
 }
